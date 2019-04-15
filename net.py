d71120363979feff6f36144466ffbcaacf200e83
masatsch <masato.11.soccer@gmail.com>
2019-04-15 14:10:59 +0900

    improve

--- net.py
+++ net.py
@@ -3,7 +3,17 @@ from functions import *
 from data import *
 
 
+class SimpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
 
+    def predict(self, x):
+        return np.dot(x, self.W)
 
+    def loss(self, x, t):
+        z = self.predict()
+        y = softmax(z)
+        loss = cross_entropy(y, t)
 
+        return loss
 
